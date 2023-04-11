b443672293376a3f102d6c9b5d69d69cd9d47230
Xavier Marquez <marquez.x@gmail.com>
2023-04-11 01:26:05 -0400

    Now tracks running cost

--- R/zorks.R
+++ R/zorks.R
@@ -1,8 +1,24 @@
 zorks <- function(prompt) {
-  zorks_resp <- openai::create_completion(model= "text-davinci-003",
+  model <- getOption("zorks.model")
+  zorks_resp <- openai::create_completion(model= model,
                                           max_tokens = 200,
                                           prompt = prompt,
                                           temperature =1)
+
+  zorks_tokens <<- zorks_tokens + zorks_resp$usage$total_tokens
+
+  if(grepl("ada", model)) {
+    approx_running_cost <<- zorks_tokens * 0.0004/1000
+  }
+  if(grepl("babbage", model)) {
+    approx_running_cost <<- zorks_tokens * 0.0005/1000
+  }
+  if(grepl("curie", model)) {
+    approx_running_cost <<- zorks_tokens * 0.0020/1000
+  }
+  if(grepl("davinci", model)) {
+    approx_running_cost <<- zorks_tokens * 0.020/1000
+  }
   zorks_resp$choices$text
 }
 
@@ -14,6 +30,8 @@ zorks <- function(prompt) {
   toset <- !(names(op.zorks) %in% names(op))
   if(any(toset)) options(op.zorks[toset])
   zorks_hist <<- character(0)
+  zorks_tokens <<- 0
+  approx_running_cost <<- 0
 
   invisible()
 
@@ -30,9 +48,17 @@ zorks <- function(prompt) {
 #' just use natural language. If you don't have an OpenAPI key, the package will
 #' prompt you for one and save it in the `OPENAI_API_KEY` environment variable.
 #'
+#' @param model The model to use. The default is "text-curie-001". But you can
+#'   use any of the GPT-3 models, including "text-davinci-003", the most
+#'   powerful. Careful! Davinci-003 can quickly get expensive.
+#'
 #' @export
 #'
-begin <- function() {
+begin <- function(model = "text-curie-001") {
+
+  options(zorks.model = model)
+  message("Using ", getOption("zorks.model"))
+
   if(is.null(Sys.getenv("OPENAI_API_KEY"))) {
     key <- readline(prompt = "Enter your OpenAI key: ")
     Sys.setenv(OPENAI_API_KEY = key)
@@ -66,12 +92,13 @@ prompt <- function(zorks_resp) {
     if(save_game == "Y") {
       writeLines(zorks_hist, "zorks.txt")
     }
-    return()
+    return(invisible())
   }
   if(!grepl("[:punct:]$", user_resp)) {
     user_resp <- paste0(user_resp, ".")
   }
   zorks_hist <<- paste(zorks_hist, zorks_resp, user_resp, sep = "\n")
-  zorks_prompt <- zorks(keep_last_n_words(zorks_hist, 800))
+  new_prompt <- keep_last_n_words(zorks_hist)
+  zorks_prompt <- zorks(new_prompt)
   prompt(zorks_prompt)
 }
--- README.md
+++ README.md
@@ -23,7 +23,8 @@ library(Zorks)
 begin()
 ```
 
+The function `begin()` has an otional `model` argument that lets you specify the model. The default is "text-curie-001", but you can specify any of the others, including "text-davinci-003". Careful! This can get expensive quickly; the game sends up to 600 tokens for each prompt.
+
 If you don't have an accesible OpenAI key, you will be prompted to enter it when you begin a game. The key is saved in the environment variable `OPENAI_API_KEY`.
 
 After you quit, the game will prompt you to save your game history; this is saved as a text file in your home directory.
-
